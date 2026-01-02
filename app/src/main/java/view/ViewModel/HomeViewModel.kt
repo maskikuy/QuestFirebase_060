cf816d9ea3fa003297c85495507acfd80b9c1375
maskikuy <kproject305@gmail.com>
2026-01-02 17:58:12 +0700

    menambahkan penyedia

--- app/src/main/java/view/ViewModel/HomeViewModel.kt
+++ app/src/main/java/view/ViewModel/HomeViewModel.kt
@@ -1,2 +1,40 @@
 package view.ViewModel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import modeldata.Siswa
+import repositori.RepositorySiswa
+import java.io.IOException
+
+sealed interface StatusUiSiswa {
+    data class Success(val siswa: List<Siswa>) : StatusUiSiswa
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
+
+class HomeViewModel(private val repositorySiswa: RepositorySiswa) : ViewModel() {
+    var statusUiSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+
+    init {
+        loadSiswa()
+    }
+
+    fun loadSiswa() {
+        viewModelScope.launch {
+            statusUiSiswa = StatusUiSiswa.Loading
+            statusUiSiswa = try {
+                // Pastikan fungsi di RepositorySiswa bernama getDataSiswa()
+                StatusUiSiswa.Success(repositorySiswa.getDataSiswa())
+            } catch (e: IOException) {
+                StatusUiSiswa.Error
+            } catch (e: Exception) {
+                StatusUiSiswa.Error
+            }
+        }
+    }
+}
